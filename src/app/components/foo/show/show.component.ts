305736d58b4fe6e6c00f4b03fd240cb5b113bce2
Maxiloud <maxime@les-tilleuls.coop>
2024-03-11 10:27:40 +0100

    show crud

--- src/app/components/foo/show/show.component.ts
+++ src/app/components/foo/show/show.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-show',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './show.component.html',
 })
 export class ShowComponent {
